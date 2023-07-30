80be8fa3773713632bb23ab58cb3339e1b1dd75a
Arun1only1 <arun1nly1@gmail.com>
2023-07-30 19:07:58 +0545

    authorization

--- user/user.route.js
+++ user/user.route.js
@@ -1,59 +1,27 @@
 import express from "express";
-import bcrypt from "bcrypt";
-import { User } from "./user.model.js";
-import jwt from "jsonwebtoken";
+import {
+  addUser,
+  authorizeUser,
+  deleteAccount,
+  getAllUsers,
+  loginUser,
+  validateUser,
+} from "./user.service.js";
 
 const router = express.Router();
 
 // register user
-// TODO:Joi validation
-// TODO:check email uniqueness
-router.post("/user/register", async (req, res) => {
-  let newUser = req.body;
 
-  const user = await User.findOne({ email: newUser.email });
-
-  if (user) {
-    return res
-      .status(409)
-      .send({ message: "User with this email already exists in our system." });
-  }
-
-  newUser.password = await bcrypt.hash(newUser.password, 8);
-
-  await User.create(newUser);
-
-  return res.status(201).send({ message: "User is registered successfully." });
-});
+router.post("/user/register", validateUser, addUser);
 
 // login user
-// TODO:validation
-router.get("/user/login", async (req, res) => {
-  const loginCredentials = req.body;
-  //   check if user exists using email
-  const user = await User.findOne({ email: loginCredentials.email });
+router.get("/user/login", loginUser);
 
-  if (!user) {
-    return res.status(401).send({ message: "Invalid credentials." });
-  }
+// get all user
+// secure route
+router.get("/users", authorizeUser, getAllUsers);
 
-  const passwordMatch = await bcrypt.compare(
-    loginCredentials.password,
-    user.password
-  );
+// delete account
+router.delete("/user/delete", authorizeUser, deleteAccount);
 
-  if (!passwordMatch) {
-    return res.status(401).send({ message: "Invalid credentials." });
-  }
-
-  //   remove password from response
-  user.password = undefined;
-
-  //   generate access token
-  const accesstoken = jwt.sign({ _id: user._id }, "djakfadfakv1234dfjkad", {
-    expiresIn: "1d",
-  });
-
-  return res.status(200).send({ user, accesstoken });
-});
 export default router;
